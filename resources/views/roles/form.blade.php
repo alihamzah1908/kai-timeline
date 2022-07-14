1ad1c1d2a1edc49d27011ec86b3ffe019e7802fa
alihamzah1908 <alihamzahar@gmail.com>
2022-07-14 18:19:17 +0700

    update css page 2022-07-14

--- resources/views/roles/form.blade.php
+++ resources/views/roles/form.blade.php
@@ -5,18 +5,15 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                <h4 class="mb-3 header-title mt-0">Create Roles</h4>
+                <h5 class="mb-3 header-title mt-0">Create Roles</h4>
                     <p class="sub-header">
-                        DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction
-                        function:
-                        <code>$().DataTable();</code>.
+                        Please insert user with form available
                     </p>
                     <form action="{{ route('roles.store') }}" method="post">
                         @csrf
                         <div class="form-group">
                             <label for="exampleInputEmail1">Name</label>
                             <input type="text" name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter role name">
-                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                         </div>
                         <div class="form-group">
                             <label for="exampleInputPassword1">Permission</label> <br />
@@ -24,7 +21,7 @@
                                 <input type="checkbox" name="permission[]" value="{{ $val->id }}"> {{ $val->name }} <br />
                             @endforeach
                         </div>
-                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="submit" class="btn btn-primary btn-rounded btn-sm">Save</button>
                     </form>
                 </div> <!-- end card body-->
             </div> <!-- end card -->
